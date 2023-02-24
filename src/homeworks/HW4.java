56a45c596768aa6908ac46fb26acb1536d54bd27
Vaiva Mineikė <vaiva.mineike@gmail.com>
2023-02-24 21:19:49 +0200

    HW, Guessnumber

--- src/homeworks/HW4.java
+++ src/homeworks/HW4.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW4 {
     public static void main(String[] args) {
         /*     Write an application, that will read two numbers from user
@@ -9,5 +11,22 @@ public class HW4 {
 
         If at least one of given numbers is equal to 0, your application
         should print false.*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter one number");
+        int input1 = scanner.nextInt();
+
+        System.out.println("Please enter second number");
+        int input2 = scanner.nextInt();
+
+        if(input1 > 0 && input2 > 0){
+            System.out.println("True");
+        } else if (input1 < 0 && input2 < 0){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
     }
 }
